67eef950e4fa06f1bbdfa729fdb104b41d65e6a3
fuka-takashima <fuka.takashima@dena.com>
2025-09-05 11:40:57 +0900

    initialcommit

--- simple-web-view/ViewController.swift
+++ simple-web-view/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let request = URLRequest(url: URL(string: "https://game.granbluefantasy.jp/")!)
+        webView.load(request)
+        webView.isInspectable = true
+
     }
 
+    @IBAction func inputUrlAlert() {
+        let alertController = UIAlertController(
+            title:"Enter URL",
+            message:"Enter the URL to open in WebView.",
+            preferredStyle: UIAlertController.Style.alert
+        )
+        alertController.addTextField()
+        let openButton = UIAlertAction(title: "Open", style: UIAlertAction.Style.default) {(action:UIAlertAction) in
+            if let textField = alertController.textFields?.first {
+                guard let url = URL(string: textField.text!) else {
+                    // invalid url
+                    return
+                }
+                self.webView.load(URLRequest(url: url))
+            }
+        }
+        alertController.addAction(openButton)
+
+        let cancelButton = UIAlertAction(title: "cancel", style: UIAlertAction.Style.cancel, handler:nil)
+        alertController.addAction(cancelButton)
+
+        present(alertController, animated:true, completion: nil)
+
+    }
 
 }
 
