a9e92f6a41503cd2753c190d04ff1d14986405ce
fuka-takashima <fuka.takashima@dena.com>
2025-09-05 09:42:33 +0900

    Initial Commit

---
+++ simple-web-view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple-web-view
+//
+//  Created by Fuka Takashima on 2025/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ simple-web-viewTests/simple_web_viewTests.swift
@@ -0,0 +1,17 @@
+//
+//  simple_web_viewTests.swift
+//  simple-web-viewTests
+//
+//  Created by Fuka Takashima on 2025/09/05.
+//
+
+import Testing
+@testable import simple_web_view
+
+struct simple_web_viewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
